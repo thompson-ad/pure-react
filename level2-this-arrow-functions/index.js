2e7c25bac31a7a2ca70ac1f1b788909de476f486
Aaron Thompson <aaron.thompson@feedagency.com>
2019-11-20 13:49:02 +0000

    level-3

--- level2-this-arrow-functions/index.js
+++ level2-this-arrow-functions/index.js
@@ -11,12 +11,18 @@ printThis();
 // we can change the value of this by using the .bind() method
 // we can pass anything to .bind() and it will create a new function that we can, say, store in a variable
 
-const fn = printThis.bind(42)
+const fn = printThis.bind(42);
 
-fn() // this will be 42
+fn(); // this will be 42
 
 // call or apply do a similar thing
 
-// 
+// whereas in an arrow function the this value is determined lexically
 
+const prinThis = () => {
+  console.log(this);
+};
 
+// there is no way to altyer the value of this with an arrow function. even when you use bind call and apply
+
+// in some cases this is great as you can pass a function around and it will mantain it;s this value but if you rquire otherwise you need a regular function
---
+++ level3-constAndLet/index.js
@@ -0,0 +1,4 @@
+// let and const are scoped to the enclosing block rather than the enclosing function
+function scopeTest() {
+    
+}
