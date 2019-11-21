652a2731c6ce9dfa8f1cec5b3f2b6f73c568da61
Aaron Thompson <aaron.thompson@feedagency.com>
2019-11-21 13:48:46 +0000

    level 6

---
+++ level4-statementsvsexpressions/index.js
@@ -0,0 +1,24 @@
+// statements vs expressions
+
+// it's important to know the difference for React because the rule is that you can only have expressions inside jsx
+
+var count = 1 + 2;  //one plus 2 here is an expression but the 
+
+if (true) { // if is an expression
+    // 
+}
+
+for() { //for is an expression and so on
+
+}
+
+while()
+
+try {
+
+} catch(e) {
+
+}
+
+
+// if we want to write an if statement as an expression we can use the TERNARY OPERATOR
---
+++ level5-templateStringsToInterpolateValuess/index.js
@@ -0,0 +1,9 @@
+const firstName = "Aaron";
+
+const lastname = "Thompson";
+
+const usesReact = true;
+
+const fullName = `${firstName} ${lastName}`;
+
+const summary = `${fullName} ${usesReact ? "usesReact" : "does not use react"}`;
